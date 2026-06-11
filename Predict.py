5bf9042f4a188b29e34b3719d7ee4f1f6a199820
diaseanodarren-hash <diaseanodarren@gmail.com>
2026-06-11 10:15:07 +0800

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -2,4 +2,8 @@ from ultralytics import YOLO
 
 model = YOLO('Nums_Model3.pt')
 
-results = model(source = 0, conf=0.50, show=True)[0]
+#conf = the minimum confidence score the model has that is shown
+#source = where the model takes in input ('0' means main webcam)
+#save = if you want to save the result
+#PLEASE ADJUST THE DATA ABOVE ACCORDING TO YOUR SYSTEM
+results = model(source = 0, conf=0.50, show=True, save=False)[0]
